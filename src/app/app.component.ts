a37396bcc822235ff2a33c49d499a1d0e0889ce8
Florian Zunke <florianzunke@gmx.de>
2025-03-19 20:55:01 +0100

    add channel-dialog, global style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChannelOverlayComponent } from "./overlays/channel-overlay/channel-overlay.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChannelOverlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/overlays/user-overlay/user-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-overlay',
+  imports: [],
+  templateUrl: './user-overlay.component.html',
+  styleUrl: './user-overlay.component.scss'
+})
+export class UserOverlayComponent {
+
+}
