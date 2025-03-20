6b29cd41c9317624c6efd0f4d5974800e0b17081
Pinguinrakete <Pinguinrakete@gmail.com>
2025-03-20 23:00:31 +0100

    add structure header

--- src/app/main/main-content/main-content.component.ts
+++ src/app/main/main-content/main-content.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarDevspaceComponent } from './sidebar-devspace/sidebar-devspace.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
+import { SidebarThreadComponent } from './sidebar-thread/sidebar-thread.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [SidebarDevspaceComponent, MessageBoxComponent, SidebarThreadComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
