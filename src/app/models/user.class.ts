4bf3e26928ef4731a65f08daefdc5e9367237135
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-20 15:48:25 +0100

    added user.class

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -1,3 +1,19 @@
 export class User {
-    
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    picture: string;
+    online: boolean; // online or offline
+    status: boolean //active or inaktiv
+
+    constructor (obj?: any) {
+      this.id = obj.id;
+      this.name = obj.name;
+      this.email = obj.email;
+      this.password = obj.password;
+      this.picture = obj.picture;
+      this.online = obj.online
+      this.status= obj.status;
+  }
 }
