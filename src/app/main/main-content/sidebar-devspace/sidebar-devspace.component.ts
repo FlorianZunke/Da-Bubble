59019369986aee82b959920c088334f4d2ced271
Pinguinrakete <Pinguinrakete@gmail.com>
2025-03-24 20:01:54 +0100

    add Devspace

--- src/app/main/main-content/sidebar-devspace/sidebar-devspace.component.ts
+++ src/app/main/main-content/sidebar-devspace/sidebar-devspace.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataService } from './../../../firebase-services/data.service';
 
 @Component({
   selector: 'app-sidebar-devspace',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar-devspace.component.html',
   styleUrl: './sidebar-devspace.component.scss'
 })
 export class SidebarDevspaceComponent {
-
+  dataService = inject(DataService);
 }
