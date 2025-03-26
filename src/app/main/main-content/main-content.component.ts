7b9bd0451fd4eb5e33ec68731842fb1fcca74d6f
Pinguinrakete <Pinguinrakete@gmail.com>
2025-03-26 15:16:13 +0100

    style new message container

--- src/app/main/main-content/main-content.component.ts
+++ src/app/main/main-content/main-content.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ToggleSidebarDevspaceComponent } from './toggle-sidebar-devspace/toggle-sidebar-devspace.component';
 import { SidebarDevspaceComponent } from './sidebar-devspace/sidebar-devspace.component';
-import { MessageBoxComponent } from './message-box/message-box.component';
+import { NewMessageComponent } from './message-box/new-message/new-message.component';
 import { SidebarThreadComponent } from './sidebar-thread/sidebar-thread.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [ToggleSidebarDevspaceComponent, SidebarDevspaceComponent, MessageBoxComponent, SidebarThreadComponent],
+  imports: [ToggleSidebarDevspaceComponent, SidebarDevspaceComponent, NewMessageComponent, SidebarThreadComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
