3c2fb97886cb378e6d9eb7b9efd20bba70e2fad3
Pinguinrakete <Pinguinrakete@gmail.com>
2025-03-27 11:40:12 +0100

    try to change

--- src/app/main/main-content/main-content.component.ts
+++ src/app/main/main-content/main-content.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { ToggleSidebarDevspaceComponent } from './toggle-sidebar-devspace/toggle-sidebar-devspace.component';
 import { SidebarDevspaceComponent } from './sidebar-devspace/sidebar-devspace.component';
-import { MessageBoxComponent } from './message-box/message-box.component';
+import { NewMessageComponent } from './message-box/new-message/new-message.component';
 import { SidebarThreadComponent } from './sidebar-thread/sidebar-thread.component';
 import { HeaderComponent } from '../header/header.component';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-content',
-  imports: [ToggleSidebarDevspaceComponent, SidebarDevspaceComponent, MessageBoxComponent, SidebarThreadComponent, HeaderComponent],
+  imports: [ToggleSidebarDevspaceComponent, SidebarDevspaceComponent, NewMessageComponent, SidebarThreadComponent, HeaderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
