8b9b93737c1f4fb05d4e4a67bb27ab88021f66db
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-03-29 08:32:09 +0100

    keyword search in user, messages, threads

--- src/app/firebase-services/message.service.ts
+++ src/app/firebase-services/message.service.ts
@@ -10,24 +10,57 @@ export class MessageService {
 
   constructor() {}
 
+  // async getAllMessages(): Promise<any[]> {
+  //   const allMessages: any[] = [];
+  //   const channelsRef = collection(this.firestore, "channels");
+  //   const channelsSnapshot = await getDocs(channelsRef);
+  //   console.log(channelsSnapshot.docs.length, 'channels found');
+
+
+  //   for (const channelDoc of channelsSnapshot.docs) {
+  //     const messagesRef = collection(channelDoc.ref, "messsages");
+  //     const messagesSnapshot = await getDocs(messagesRef);
+  //     console.log(messagesSnapshot.docs.length, 'messages found in channel', channelDoc.id);
+
+
+  //     for (const messageDoc of messagesSnapshot.docs) {
+  //       const messageData = messageDoc.data();
+  //       allMessages.push(messageData);
+  //       console.log(messageData, 'message found in channel', channelDoc.id);
+
+  //     }
+  //   }
+  //   return allMessages;
+  // }
+
   async getAllMessages(): Promise<any[]> {
     const allMessages: any[] = [];
     const channelsRef = collection(this.firestore, "channels");
     const channelsSnapshot = await getDocs(channelsRef);
     console.log(channelsSnapshot.docs.length, 'channels found');
 
-
     for (const channelDoc of channelsSnapshot.docs) {
       const messagesRef = collection(channelDoc.ref, "messsages");
       const messagesSnapshot = await getDocs(messagesRef);
       console.log(messagesSnapshot.docs.length, 'messages found in channel', channelDoc.id);
 
-
       for (const messageDoc of messagesSnapshot.docs) {
         const messageData = messageDoc.data();
+        messageData['id'] = messageDoc.id;
         allMessages.push(messageData);
         console.log(messageData, 'message found in channel', channelDoc.id);
 
+        const threadsRef = collection(messageDoc.ref, "thread");
+        const threadsSnapshot = await getDocs(threadsRef);
+        console.log(threadsSnapshot.docs.length, 'threads found in message', messageDoc.id);
+
+        for (const threadDoc of threadsSnapshot.docs) {
+          const threadMessageData = threadDoc.data();
+          threadMessageData['id'] = threadDoc.id;
+          threadMessageData['parentMessageId'] = messageDoc.id; // Referenz zur ursprünglichen Nachricht
+          allMessages.push(threadMessageData);
+          console.log(threadMessageData, 'thread message found in message', messageDoc.id);
+        }
       }
     }
     return allMessages;
