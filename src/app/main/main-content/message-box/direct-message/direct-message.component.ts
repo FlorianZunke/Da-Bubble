8ba223cfa86c49193b197e42fcdcafc8cb5ec48e
Pinguinrakete <Pinguinrakete@gmail.com>
2025-03-29 10:37:20 +0100

    textarea outgesourced

--- src/app/main/main-content/message-box/direct-message/direct-message.component.ts
+++ src/app/main/main-content/message-box/direct-message/direct-message.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from './../../../../firebase-services/data.service';
+import { TextareaComponent } from '../textarea/textarea.component';
 
 @Component({
   selector: 'app-direct-message',
-  imports: [],
+  imports: [TextareaComponent],
   templateUrl: './direct-message.component.html',
   styleUrl: './direct-message.component.scss'
 })
---
+++ src/app/main/main-content/message-box/textarea/textarea.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-textarea',
+  imports: [CommonModule],
+  templateUrl: './textarea.component.html',
+  styleUrl: './textarea.component.scss'
+})
+export class TextareaComponent {
+  @Input() mainMessageBoxPadding: string = '2.5rem 2.8125rem 2.5rem 2.8125rem';
+  @Input() toolbarWidth: string = 'calc(100% - 8.125rem)';
+}
