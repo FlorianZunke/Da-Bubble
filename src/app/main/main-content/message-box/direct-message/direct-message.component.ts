d3c21b69ec4ced748a5bd26428c8d1bc26dd98b5
Pinguinrakete <Pinguinrakete@gmail.com>
2025-03-29 15:03:27 +0100

    my own message

--- src/app/main/main-content/message-box/direct-message/direct-message.component.ts
+++ src/app/main/main-content/message-box/direct-message/direct-message.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DataService } from './../../../../firebase-services/data.service';
 import { TextareaComponent } from '../textarea/textarea.component';
 
 @Component({
   selector: 'app-direct-message',
-  imports: [TextareaComponent],
+  imports: [CommonModule ,TextareaComponent],
   templateUrl: './direct-message.component.html',
   styleUrl: './direct-message.component.scss'
 })
--- src/app/main/main-content/message-box/textarea/textarea.component.ts
+++ src/app/main/main-content/message-box/textarea/textarea.component.ts
@@ -10,4 +10,5 @@ import { CommonModule } from '@angular/common';
 export class TextareaComponent {
   @Input() mainMessageBoxPadding: string = '2.5rem 2.8125rem 2.5rem 2.8125rem';
   @Input() toolbarWidth: string = 'calc(100% - 8.125rem)';
+  @Input() placeholder: string = '';
 }
