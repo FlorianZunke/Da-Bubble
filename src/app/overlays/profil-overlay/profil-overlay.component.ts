a5e26b2b749487bb45067ffb93c4a9942ac20ac4
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-04-03 10:36:20 +0200

    user profil load user data, green dot when online, grey when away/offline

--- src/app/overlays/profil-overlay/profil-overlay.component.ts
+++ src/app/overlays/profil-overlay/profil-overlay.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { DataService } from '../../firebase-services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profil-overlay',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule,CommonModule],
   templateUrl: './profil-overlay.component.html',
   styleUrl: './profil-overlay.component.scss'
 })
 export class ProfilOverlayComponent {
+  logedUser: any;
+
+  constructor (private dataService : DataService) {
+    this.dataService.logedUser$.subscribe(user => {
+      this.logedUser = user;
+    });
+  }
 
 }
