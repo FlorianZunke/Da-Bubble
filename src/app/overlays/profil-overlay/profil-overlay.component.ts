bcb98dbd741e8e093d2f1f584ec4ebbb70140fbd
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-04-04 13:52:47 +0200

    start edit profile dialog

--- src/app/overlays/profil-overlay/profil-overlay.component.ts
+++ src/app/overlays/profil-overlay/profil-overlay.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { DataService } from '../../firebase-services/data.service';
 import { CommonModule } from '@angular/common';
+import { EditProfileComponent } from '../edit-profile/edit-profile.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-profil-overlay',
@@ -12,11 +14,40 @@ import { CommonModule } from '@angular/common';
 })
 export class ProfilOverlayComponent {
   logedUser: any;
+  readonly dialog = inject(MatDialog);
 
   constructor (private dataService : DataService) {
     this.dataService.logedUser$.subscribe(user => {
       this.logedUser = user;
     });
+  }
+
+  openEditProfilDialog(event: MouseEvent) {
+    const target = event.target as HTMLElement; // Klick-Element (das <img>)
+    const rect = target.getBoundingClientRect(); // Position ermitteln
+    const dialogHeight = 70;
+    const dialogWidth = 137;
+
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 1920) {
+      this.dialog.open(EditProfileComponent, {
+        position: {
+          top: `${rect.bottom - dialogHeight + window.scrollY}px`, // Unterhalb des Bildes öffnen
+          // left: `${rect.left - dialogWidth + window.scrollX}px`, // Gleiche X-Position wie das Bild
+            right: `1rem`, //bei screen width 1920 px
+        },
+        panelClass: 'custom-dialog', // Falls du CSS-Anpassungen machen willst
+      });
+    } else {
+      this.dialog.open(EditProfileComponent, {
+        position: {
+          top: `${rect.bottom - dialogHeight + window.scrollY}px`, // Unterhalb des Bildes öffnen
+          // left: `${rect.left - dialogWidth + window.scrollX}px`, // Gleiche X-Position wie das Bild
+            right: `5rem`, //bei screen width > 1920 px, hier mus gerne eine genaue formel hin
+        },
+        panelClass: 'custom-dialog', // Falls du CSS-Anpassungen machen willst
+      });
+    }
   }
 
 }
