e0835fa5ea0a9c0f28f18125d24552dc259783d6
enesesine <eneslucker@gmail.com>
2025-04-04 21:36:41 +0200

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
 import { MainContentComponent } from './main/main-content/main-content.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { SignInComponent } from './features/sign-in/sign-in.component';
-import { ImpressumComponent } from './features/impressum/impressum.component';
-import { DatenschutzComponent } from './features/datenschutz/datenschutz.component';
-import { ResetPasswordComponent } from './features/reset-password/reset-password.component';
+import { ChannelMessageComponent } from './main/main-content/message-box/channel-message/channel-message.component';
+import { DirectMessageComponent } from './main/main-content/message-box/direct-message/direct-message.component';
+import { NewMessageComponent } from './main/main-content/message-box/new-message/new-message.component';
 
 export const routes: Routes = [
   { path: '', component: SignInComponent },
@@ -15,4 +15,7 @@ export const routes: Routes = [
   { path: 'choose-avatar', component: ChooseAvatarComponent },
   { path: 'login', component: SignInComponent },
   { path: 'legalNotice', component: LegalNoticeComponent },
+  { path: 'channelMessages', component: ChannelMessageComponent },
+  { path: 'privateMessages', component: DirectMessageComponent },
+  { path: 'newMessage', component: NewMessageComponent },
 ];
