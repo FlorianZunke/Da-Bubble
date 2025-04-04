bcc35163dc9cbaaf6645228080c6207d7d854763
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-04-04 12:17:47 +0200

    start styling user profil overlay

--- src/app/overlays/user-drop-menu/user-drop-menu.component.ts
+++ src/app/overlays/user-drop-menu/user-drop-menu.component.ts
@@ -33,13 +33,35 @@ export class UserDropMenuComponent {
     const dialogHeight = 70;
     const dialogWidth = 137;
 
-    this.dialog.open(ProfilOverlayComponent, {
-      position: {
-        top: `${rect.bottom - dialogHeight + window.scrollY}px`, // Unterhalb des Bildes öffnen
-        left: `${rect.left - dialogWidth + window.scrollX}px`, // Gleiche X-Position wie das Bild
-      },
-      panelClass: 'custom-dialog', // Falls du CSS-Anpassungen machen willst
-    });
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 1920) {
+      this.dialog.open(ProfilOverlayComponent, {
+        position: {
+          top: `${rect.bottom - dialogHeight + window.scrollY}px`, // Unterhalb des Bildes öffnen
+          // left: `${rect.left - dialogWidth + window.scrollX}px`, // Gleiche X-Position wie das Bild
+            right: `1rem`, //bei screen width 1920 px
+        },
+        panelClass: 'custom-dialog', // Falls du CSS-Anpassungen machen willst
+      });
+    } else {
+      this.dialog.open(ProfilOverlayComponent, {
+        position: {
+          top: `${rect.bottom - dialogHeight + window.scrollY}px`, // Unterhalb des Bildes öffnen
+          // left: `${rect.left - dialogWidth + window.scrollX}px`, // Gleiche X-Position wie das Bild
+            right: `5rem`, //bei screen width > 1920 px, hier mus gerne eine genaue formel hin
+        },
+        panelClass: 'custom-dialog', // Falls du CSS-Anpassungen machen willst
+      });
+    }
+
+    // this.dialog.open(ProfilOverlayComponent, {
+    //   position: {
+    //     top: `${rect.bottom - dialogHeight + window.scrollY}px`, // Unterhalb des Bildes öffnen
+    //     // left: `${rect.left - dialogWidth + window.scrollX}px`, // Gleiche X-Position wie das Bild
+    //       right: `1rem`, //bei screen width 1920 px
+    //   },
+    //   panelClass: 'custom-dialog', // Falls du CSS-Anpassungen machen willst
+    // });
   }
 
   closeDialog() {
