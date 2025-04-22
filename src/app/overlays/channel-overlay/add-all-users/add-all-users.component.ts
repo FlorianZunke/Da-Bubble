7456deb55a47ec8eb493cef491fd633453f3eca1
Pinguinrakete <Pinguinrakete@gmail.com>
2025-04-22 23:01:15 +0200

    generate add-all-users and add-selected-users

---
+++ src/app/overlays/channel-overlay/add-all-users/add-all-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-all-users',
+  imports: [],
+  templateUrl: './add-all-users.component.html',
+  styleUrl: './add-all-users.component.scss'
+})
+export class AddAllUsersComponent {
+
+}
