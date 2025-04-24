850745f5642aad637bb11ccab2c490d697632880
Pinguinrakete <Pinguinrakete@gmail.com>
2025-04-24 23:08:35 +0200

    add members to channel

--- src/app/overlays/channel-overlay/add-all-users/add-all-users.component.ts
+++ src/app/overlays/channel-overlay/add-all-users/add-all-users.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-all-users',
-  imports: [],
+  imports: [CommonModule, FormsModule, MatRadioModule, MatDialogModule],
   templateUrl: './add-all-users.component.html',
   styleUrl: './add-all-users.component.scss'
 })
 export class AddAllUsersComponent {
+  showInput = false;
 
+  constructor(
+    private dialog: MatDialog
+  ) { }
+
+  toggleInput() {
+    this.showInput = !this.showInput;
+  }
+  
 }
