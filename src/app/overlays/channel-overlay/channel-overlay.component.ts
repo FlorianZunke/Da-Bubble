8c6a0b74b959bdb70c666254dc99c1e8e720ae0a
Pinguinrakete <Pinguinrakete@gmail.com>
2025-04-30 14:53:12 +0200

    form validation add channel name exists

--- src/app/overlays/channel-overlay/channel-overlay.component.ts
+++ src/app/overlays/channel-overlay/channel-overlay.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Channel } from '../../models/channel.class';
+import { ChannelService } from '../../firebase-services/channel.service';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { Channel } from '../../models/channel.class';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AddAllUsersComponent } from './add-all-users/add-all-users.component';
 
@@ -13,18 +14,36 @@ import { AddAllUsersComponent } from './add-all-users/add-all-users.component';
   styleUrl: './channel-overlay.component.scss'
 })
 export class ChannelOverlayComponent {
-
+  channels: any[] = [];
   channel: Channel = new Channel;
+  channelExists: boolean = false;
 
   constructor(
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private channelService: ChannelService,
   ) { }
 
-  openAddAllUsers(): void {
-      this.dialog.open(AddAllUsersComponent, {
-        panelClass: 'add-user-container',
-        data: { channel: this.channel }
-      });
-    }
-}
+  ngOnInit() {
+    this.channelService.channels$.subscribe((channels) => {
+      this.channels = channels; 
+    });
+  }
 
+  checkChannelExists(): void {
+    this.channelExists = false;
+    for (let i = 0; i < this.channels.length; i++) {
+      if(this.channel.channelName === this.channels[i]['channelName']) {
+        console.log('Treffer');
+        this.channelExists = true;
+      } 
+    }
+  } 
+
+
+  openAddAllUsers(): void {
+    this.dialog.open(AddAllUsersComponent, {
+      panelClass: 'add-user-container',
+      data: { channel: this.channel }
+    });
+  }
+}
