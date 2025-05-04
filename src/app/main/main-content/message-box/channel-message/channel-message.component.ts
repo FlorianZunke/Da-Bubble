a8cbd88ce3c8a1e3fad5ddc9bff184f23096c84c
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-05-04 23:42:27 +0200

    update emoji reaction chnanel message to firebase

--- src/app/main/main-content/message-box/channel-message/channel-message.component.ts
+++ src/app/main/main-content/message-box/channel-message/channel-message.component.ts
@@ -218,6 +218,9 @@ export class ChannelMessageComponent implements OnInit, OnDestroy {
     if (!msg.reactions.includes(emoji)) {
       msg.reactions.push(emoji);
       // TODO: Persist via channelService.updateMessageReactions(...)
+      console.log(this.currentChannel);
+
+      this.channelService.updateMessageReactions(this.currentChannel?.id || '', msg.id, emoji);
     }
     this.reactionPickerMessageId = null;
   }
